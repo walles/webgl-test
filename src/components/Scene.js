c9d667ed9909bcd21f132416eb461ea5ea360bad
Johan Walles <johan.walles@gmail.com>
2018-02-28 22:37:16 +0100

    Make the cube prettier

--- src/components/Scene.js
+++ src/components/Scene.js
@@ -15,19 +15,29 @@ class Scene extends Component {
     const height = this.mount.clientHeight;
 
     const scene = new THREE.Scene();
+
     const camera = new THREE.PerspectiveCamera(
       75,
       width / height,
       0.1,
       1000
     );
+    camera.position.z = 4;
+
     const renderer = new THREE.WebGLRenderer({ antialias: true });
     const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({ color: '#433F81' });
-    const cube = new THREE.Mesh(geometry, material);
+    const material = new THREE.MeshLambertMaterial({ color: '#00ff00' });
 
-    camera.position.z = 4;
+    const cube = new THREE.Mesh(geometry, material);
     scene.add(cube);
+
+    // create a point light
+    const pointLight = new THREE.PointLight(0xFFFFFF);
+    pointLight.position.x = 1;
+    pointLight.position.y = 0;
+    pointLight.position.z = 3;
+    scene.add(pointLight);
+
     renderer.setClearColor('#000000');
     renderer.setSize(width, height);
 
@@ -58,7 +68,7 @@ class Scene extends Component {
 
   animate() {
     this.cube.rotation.x += 0.01;
-    this.cube.rotation.y += 0.01;
+    this.cube.rotation.y += 0.02;
 
     this.renderScene();
     this.frameId = window.requestAnimationFrame(this.animate);
