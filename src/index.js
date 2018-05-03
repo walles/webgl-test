88c72da05e30464b9c000376ffa47d2df77b9de3
Johan Walles <johan.walles@gmail.com>
2018-05-03 18:28:19 +0200

    Use the full window for the WebGL rendering

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Scene from './components/Scene';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Scene />, document.getElementById('root'));
 registerServiceWorker();
